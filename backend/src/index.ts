cf1e900e350cbd7c4c1804a2b0674c076e6161d8
Kemjiga <kemerjg@gmail.com>
2025-07-02 17:03:20 -0500

    add reqs limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import db from './db/db';
+import rateLimit from 'express-rate-limit';
 
 import urlRoutes from './routes/urlRoutes';
 
@@ -10,6 +11,12 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 min
+    max: 100, // max 100 requests per window per IP
+  })
+);
 
 app.use('/', urlRoutes);
 
